e772c8cca50a2d2e6b71611d074d21533e8b1c2e
Cadovvl <pavelandreevith@gmail.com>
2016-09-12 09:36:25 +0300

    Update get-zoo.sh

--- get-zoo.sh
+++ get-zoo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo -e "\e[1;33m"
-wget  http://apache-mirror.rbc.ru/pub/apache/zookeeper/zookeeper-3.4.7/zookeeper-3.4.7.tar.gz
+wget  http://apache-mirror.rbc.ru/pub/apache/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz
 echo 'Unpacking zookeeper'
-mkdir zookeeper && tar -xf zookeeper-3.4.7.tar.gz -C ./zookeeper --strip-components 1
+mkdir zookeeper && tar -xf zookeeper-3.4.8.tar.gz -C ./zookeeper --strip-components 1
 echo 'Clear temp file'
-rm ./zookeeper-3.4.7.tar.gz
+rm ./zookeeper-3.4.8.tar.gz
 echo -e "\e[39m"
