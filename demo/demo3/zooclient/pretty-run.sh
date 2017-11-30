249c998cd9d462131ac15986ff6d4b0bfea6c9d3
Sukhov Pavel <pasukhov@yandex-team.ru>
2017-11-30 22:49:39 +0300

    new stable version

--- demo/demo3/zooclient/pretty-run.sh
+++ demo/demo3/zooclient/pretty-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac Main.java -cp ../../../zookeeper/zookeeper-3.4.7.jar
+javac Main.java -cp ../../../zookeeper/zookeeper-3.4.11.jar
 
-java -cp .:../../../zookeeper/zookeeper-3.4.7.jar:../../../zookeeper/lib/* Main 2>&1 | ../../../re_colorize.sh red '^x .*' blue '^+ .*' yellow '^- .*'
+java -cp .:../../../zookeeper/zookeeper-3.4.11.jar:../../../zookeeper/lib/* Main 2>&1 | ../../../re_colorize.sh red '^x .*' blue '^+ .*' yellow '^- .*'
 
